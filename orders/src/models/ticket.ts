6a10a1c36bac93152f79771855ebd25289888ca6
Daniel-Conte <danielmconte.hotmail.com>
2021-09-16 14:33:48 -0700

    orders tests

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
     title: string;
@@ -8,13 +9,14 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
+    isReserved(): Promise<boolean> 
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
     build(attrs: TicketAttrs): TicketDoc;
 }
 
-const ticketSchema = new mongoose.Schema({
+const ticketSchema = new mongoose.Schema<TicketDoc>({
   title: {
       type: String,
       required: true
@@ -35,7 +37,23 @@ const ticketSchema = new mongoose.Schema({
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
-}
+};
+
+ticketSchema.methods.isReserved = async function() {
+    const existingOrder = await Order.findOne({
+        ticket: this,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete
+            ]
+        }
+    })
+    // This is in case existingOrder is null will flip to true, then fasle. 
+    return !!existingOrder;
+};
+
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
