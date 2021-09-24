d9ccfb2e4032269ac36f0e5997918d732a0adcb7
Daniel-Conte <danielmconte.hotmail.com>
2021-09-24 16:05:05 -0700

    ticket reserve

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
@@ -7,8 +8,9 @@ import { natsWrapper } from '../../nats-wrapper';
 it('marks an order as cancelled', async () => {
     // create a ticket with Ticket Model
     const ticket = Ticket.build({
+        id: mongoose.Types.ObjectId().toHexString(),
         title: 'concert',
-        price: 20
+        price: 20,
     });
     await ticket.save();
     const user = global.signin();
@@ -34,6 +36,7 @@ it('marks an order as cancelled', async () => {
 
 it('emits an order cancelled event', async () => {
     const ticket = Ticket.build({
+        id: mongoose.Types.ObjectId().toHexString(),
         title: 'concert',
         price: 20
     });
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -14,6 +14,7 @@ interface TicketDoc extends mongoose.Document {
     price: number;
     userId: string;
     version: number;
+    orderId?: string;
 }
 
 
@@ -33,6 +34,9 @@ const ticketSchema = new mongoose.Schema({
     userId: {
         type: String,
         required: true
+    },
+    orderId: {
+        type: String, 
     }
 }, {
     toJSON: {
